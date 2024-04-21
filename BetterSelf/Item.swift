0f10dd5538c4058da5cb6c27e391421a0f2426b7
DDreame <x814061923@qq.com>
2024-04-21 17:11:43 +0800

    Initial Commit

---
+++ BetterSelf/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BetterSelf
+//
+//  Created by 辛晨 on 2024/4/21.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
