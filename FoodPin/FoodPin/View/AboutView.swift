c00dce179cdcddbd8745dc5f82b905285a6bbc1e
DDreame <x814061923@qq.com>
2024-05-09 18:22:49 +0800

    update foodpin: webview

--- FoodPin/FoodPin/View/AboutView.swift
+++ FoodPin/FoodPin/View/AboutView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    @State private var link: WebLink?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+             List {
+                 Image("about")
+                     .resizable()
+                     .scaledToFit()
+
+                 Section {
+                     Link(destination: URL(string: WebLink.rateUs.rawValue)!, label: {
+                         Label("Rate us on App Store", image: "store")
+                             .foregroundStyle(.primary)
+                     })
+
+                     Label("Tell us your feedback", image: "chat")
+                         .onTapGesture {
+                            link = .feedback
+                        }
+                 }
+
+                 Section {
+                     Label("Twitter", image: "twitter")
+                         .onTapGesture {
+                            link = .twitter
+                        }
+
+                     Label("Facebook", image: "facebook")
+                         .onTapGesture {
+                            link = .facebook
+                        }
+
+                     Label("Instagram", image: "instagram")
+                         .onTapGesture {
+                            link = .instagram
+                        }
+                 }
+             }
+             .listStyle(.grouped)
+             .navigationTitle("About")
+             .navigationBarTitleDisplayMode(.automatic)
+             .sheet(item: $link) { item in
+                 if let url = URL(string: item.rawValue) {
+                     WebView(url: url)
+                    // SafariView(url: url)
+                 }
+             }
+         }
+        
+    }
+    
+    enum WebLink: String, Identifiable{
+        case rateUs = "https://www.apple.com/ios/app-store"
+        case feedback = "https://www.appcoda.com/contact"
+        case twitter = "https://www.twitter.com/appcodamobile"
+        case facebook = "https://www.facebook.com/appcodamobile"
+        case instagram = "https://www.instagram.com/appcodadotcom"
+        var id: UUID {
+                UUID()
+            }
     }
 }
 
---
+++ FoodPin/FoodPin/View/WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  FoodPin
+//
+//  Created by DDreame on 2024/5/9.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    var url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+//#Preview {
+//    WebView()
+//}
